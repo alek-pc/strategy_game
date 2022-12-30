c46d0c8d0227619c3faaca559536afbe326deabd
alek-pc <101040127+alek-pc@users.noreply.github.com>
2022-12-30 23:19:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from random import randrange, random
 
 BASE_CHARACTERISTICS = {}
-level = 1   # уровень сложности
+level = 1  # уровень сложности
 
 
 class Area:  # область
-    def __init__(self, country, resources):  # страна к которой относится, ресурсы
+    def __init__(self, country):  # страна к которой относится, ресурсы
         self.country = country
-        self.resources = resources
         self.buildings = []
         # вероятность появления, пределы распространенности
         self.probabilities = {'gold': [0.1, 100, 1000], 'forest': [0.85, 500, 40_000], 'soil': [0.85, 10, 40],
@@ -41,11 +40,11 @@ class Area:  # область
                 self.characteristics[key] = round(self.characteristics[key] * 1.007)
 
         for building in self.buildings:  # проходимся по всем зданиям
-
             if building.get_class() != 'University':
                 self.characteristics[building.next_turn()[0]] += building.next_turn()[2]
-            # + в добытые ресурсы
-            self.characteristics[building.next_turn()[1]] += abs(building.next_turn()[2])
+            if building.get_class() != 'ArmyAcademy':
+                # + в добытые ресурсы
+                self.characteristics[building.next_turn()[1]] += abs(building.next_turn()[2])
 
     def build_building(self, building):  # добавить здание: класс здания
         if building.get_class() not in [i.get_class() for i in self.buildings]:  # первое здание - бесплатно
@@ -115,17 +114,18 @@ class Building:
 
 class University(Building):
     price = {'wood': 1000, 'processed_iron': 50, 'processed_gold': 10, 'science': 25}  # цена постройки
+    data = {'science': 10, 'level': 1, 'years': 5}
 
     def __init__(self, area):
         self.name = 'University'
-        self.data = {'science': 10, 'level': 1, 'years': 5}
-        self.year = 0
+        self.year = 1
+        self.area = area
 
     def next_turn(self):
         if self.year % self.data['years'] == 0:  # правители появляются раз в несколько лет
             for _ in range(self.data['level']):  # в зависимости от уровня университета, увеличивается
                 # количество выпускаемых правителей
-                pass  # создание правителей
+                Governor(self.area)  # создание правителей
         self.year += 1
         return 'science', 'science', self.data['science']
 
@@ -137,97 +137,97 @@ class University(Building):
 
 class GoldMine(Building):
     price = {'wood': 300, 'iron': 15, 'science': 10}
+    data = {'gold': 10, 'level': 1}
 
     def __init__(self, area):
-        self.data = {'mining': 10, 'level': 1}
         self.area = area
         self.name = 'GoldMine'
 
     def next_turn(self):
         if self.area.characteristics['gold']:
-            if self.area.characteristics['gold'] < self.data['mining']:
+            if self.area.characteristics['gold'] < self.data['gold']:
                 gold = self.area.characteristics['gold']
                 return 'gold', 'extracted_gold', -gold
             else:
-                return 'gold', 'extracted_gold', -self.data['mining']
+                return 'gold', 'extracted_gold', -self.data['gold']
         return 'gold', 'extracted_gold', 0
 
     def level_up(self):
         if self.data['level'] < 3:
             self.data['level'] += 1
-            self.data['mining'] += 5
+            self.data['gold'] += 5
 
 
 class IronMine(Building):
     price = {'wood': 300, 'iron': 15, 'science': 10}
+    data = {'iron': 25, 'level': 1}
 
     def __init__(self, area):
         self.area = area
-        self.data = {'mining': 25, 'level': 1}
         self.name = 'IronMine'
 
     def next_turn(self):
         if self.area.characteristics['iron']:
-            if self.area.characteristics['iron'] < self.data['mining']:
+            if self.area.characteristics['iron'] < self.data['iron']:
                 iron = self.area.characteristics['iron']
                 return 'iron', 'extracted_iron', -iron
-            return 'iron', 'extracted_iron', -self.data['mining']
+            return 'iron', 'extracted_iron', -self.data['iron']
         return 'iron', 'extracted_iron', 0
 
     def level_up(self):
         if self.data['level'] < 3:
             self.data['level'] += 1
-            self.data['mining'] += 20
+            self.data['iron'] += 20
 
 
 class Sawmill(Building):
-    price = {'wood': 300, 'extracted_iron': 30, 'science': 3}
+    price = {'wood': 300, 'extracted_iron': 30, 'science': 10}
+    data = {'forest': 100, 'level': 1}
 
     def __init__(self, area):
-        self.data = {'mining': 100, 'level': 1}
         self.area = area
         self.name = 'Sawmill'
 
     def next_turn(self):
         if self.area.characteristics['forest']:
-            if self.data['mining'] > self.area.characteristics['forest']:
+            if self.data['forest'] > self.area.characteristics['forest']:
                 return 'forest', 'wood', -self.area.characteristics['forest']
-            return 'forest', 'wood', -self.data['mining']
+            return 'forest', 'wood', -self.data['forest']
         return 'forest', 'wood', 0
 
     def level_up(self):
         if self.data['level'] < 3:
-            self.data['mining'] += 100
+            self.data['forest'] += 100
             self.data['level'] += 1
 
 
 class MetallurgicalPlant(Building):
     price = {'extracted_iron': 70, 'wood': 1000, 'science': 40}
+    data = {'level': 1, 'extracted_iron': 20}
 
     def __init__(self, area):
-        self.data = {'level': 1, 'processing': 20}
         self.area = area
         self.name = 'MetallurgicalPlant'
 
     def next_turn(self):
         if self.area.characteristics['extracted_iron']:
-            if self.area.characteristics['extracted_iron'] < self.data['processing']:
+            if self.area.characteristics['extracted_iron'] < self.data['extracted_iron']:
                 return 'extracted_iron', 'processed_iron', -self.area.characteristics['extracted_iron']
-            return 'extracted_iron', 'processed_iron', -self.data['processing']
+            return 'extracted_iron', 'processed_iron', -self.data['extracted_iron']
         return 'extracted_iron', 'processed_iron', 0
 
     def level_up(self):
         if self.data['level'] < 3:
             self.data['level'] += 1
-            self.data['processing'] += 20
+            self.data['extracted_iron'] += 20
 
 
 class ArmyAcademy(Building):
-    price = {'wood': 1000, 'processed_iron': 15, 'science': 30}
+    price = {'wood': 1000, 'processed_iron': 15, 'science': 40}
+    data = {'years': 5, 'level': 1, 'science': 10}
 
     def __init__(self, area):
         self.name = 'ArmyAcademy'
-        self.data = {'years': 5, 'level': 1, 'science': 10}
         self.year = 0
         self.area = area
 
@@ -246,21 +246,46 @@ class ArmyAcademy(Building):
             self.data['science'] += 5
 
 
+class Governor:
+    def __init__(self, area):
+        self.characteristics = {'loyalty': randrange(75 - (level - 1) * 10, 90 - (level - 1) * 10),
+                                'intellect': randrange(65 - (level - 1) * 10, 80 - (level - 1) * 10),
+                                'honesty': randrange(80 - (level - 1) * 10, 100 - (level - 1) * 10)}
+        self.area = area
+
+    def next_turn(self):
+        # строительство здания
+        for building in [University, GoldMine, IronMine, Sawmill, MetallurgicalPlant]:
+            if ((building(self.area).get_class() not in [i.get_class() for i in self.area.buildings] or
+                 all([self.area.country.characteristics[i] >= k for i, k in building.price.items()])) and
+                    random() * 100 <= self.characteristics['intellect'] and
+                    self.area.characteristics[[i for i in list(building.data.keys()) if i != 'year' != 'level'][0]] >=
+                    building.data[[i for i in list(building.data.keys()) if i != 'year' != 'level'][0]]):
+                self.area.build_building(building(self.area))
+
+        for building in self.area.buildings:  # проходимся по всем зданиям
+            if building.get_class() != 'University':
+                # то, сколько минусуется ресурсов не зависит от честности правителя
+                self.area.characteristics[building.next_turn()[0]] += building.next_turn()[2]
+            if building.get_class() != 'ArmyAcademy':
+                # + в добытые ресурсы, НО не все, тк это зависит от честности правителя
+                self.area.characteristics[building.next_turn()[1]] += round(abs(building.next_turn()[2]) *
+                                                                            self.characteristics['honesty'] * 0.01)
+
+
 if __name__ == '__main__':
     b = Country('Russia')
-    a = Area(b, [])
+    a = Area(b)
     b.add_area(a)
-    print()
     a.build_building(University(a))
     a.build_building(GoldMine(a))
     a.build_building(IronMine(a))
     a.build_building(Sawmill(a))
+    a.build_building(ArmyAcademy(a))
 
     b.next_turn()
     print(b.get_characteristics())
     a.build_building(MetallurgicalPlant(a))
     while not input():
         b.next_turn()
-        print(b.get_characteristics())
-        print(a.build_building(Sawmill(a)))
         print(b.get_characteristics())
